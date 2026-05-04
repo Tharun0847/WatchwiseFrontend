1a10e272fdc59671aa1c4f1f46ceb61a382bf9ac
Tharun M <myselftharun8@gmail.com>
2026-05-04 13:24:49 +0530

    ch

--- Fn/src/services/watchlistAPI.js
+++ Fn/src/services/watchlistAPI.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { API_BASE_URL } from "../config";
 
-const API_URL = `${API_BASE_URL}/watchlist`;
+// const API_URL = `${API_BASE_URL}/watchlist`;
 
-export const addToWatchlist = (item) => axios.post(`${API_URL}/add`, item);
-export const getWatchlist = (userId) => axios.get(`${API_URL}/${userId}`);
-export const removeFromWatchlist = (id) => axios.delete(`${API_URL}/remove/${id}`);
-export const updateWatchlistStatus = (id, status) => axios.put(`${API_URL}/update-status/${id}`, { status });
+export const addToWatchlist = (item) => axios.post(`${API_BASE_URL}/watchlist/add`, item);
+export const getWatchlist = (userId) => axios.get(`${API_BASE_URL}/watchlist/${userId}`);
+export const removeFromWatchlist = (id) => axios.delete(`${API_BASE_URL}/watchlist/remove/${id}`);
+export const updateWatchlistStatus = (id, status) => axios.put(`${API_BASE_URL}/watchlist/update-status/${id}`, { status });
